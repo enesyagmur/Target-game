632a44e0e89ce5154830f4b35bf6200e529f1e01
Enes Yağmur <106833747+enesrain@users.noreply.github.com>
2023-01-23 01:02:38 +0300

    part2

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+
+alert("Oyunu Başlat")
+
 var a;
-var repeat = 1000;
+var heart=3;
+var repeat=1000;
 var finish=1000;
 
+
 let time = setInterval(function(){
-    var rnd = Math.floor(Math.random()*3);
+    var rnd = Math.floor(Math.random()*7);
      a = rnd;
      
     if(rnd==1){
@@ -57,16 +62,49 @@ let time = setInterval(function(){
     
 },repeat);
 
-document.querySelector(".taskafa1").addEventListener("click",function(){
-  if(a==1){
- 
-   console.log("vurdun");
-    clearInterval(time);
 
-  }
- 
- 
- 
- })
 
- 
+
+function check(x,y){
+  document.querySelector(x).addEventListener("click",function(){
+    if(a==y){
+      clearInterval(time);
+     document.querySelector("body").style.backgroundColor="green";
+     document.querySelector("h1").innerHTML="Oyun Bitti:Uzaylıyı Vurdun";
+
+     
+    }
+    else{
+     heart--;
+     if(heart==2){
+      document.querySelector("#heart3").style.color="white";
+     }
+     else if(heart==1){
+      document.querySelector("#heart2").style.color="white";
+     }
+     else if(heart==0){
+      document.querySelector("#heart1").style.color="white";
+      clearInterval(time);
+     document.querySelector("h1").innerHTML="Oyun Bitti:Uzaylıyı Vuramadın";
+     document.querySelector("body").style.backgroundColor="red";
+     document.querySelector("#heart3").style.color="red";
+     document.querySelector("#heart2").style.color="red";
+     document.querySelector("#heart1").style.color="red";
+
+
+     
+     }
+       
+    }
+  })
+}
+
+check(".taskafa1",1);
+check(".taskafa2",2);
+check(".taskafa3",3);
+check(".taskafa4",5);
+check(".taskafa5",5);
+check(".taskafa6",6);
+
+
+
